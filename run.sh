d74de12435328f3f30ba801394dd23ed4b245de7
Ilya Esin <iyesin@users.noreply.github.com>
2019-08-12 03:34:25 +0200

    Huge Docker image update (#25)
    * Trivial base image version bump
    * Use modern way to initialize MySQL DB
    * \n in the end of file. Space-at-EOL removed
    * Group all volumes in one command
    * Move environment variables to top
    * Move imported files to top
    * Merge ADD commands for /*.sh scripts
    * DEBIAN_FRONTEND environment variable defined now globally
    * No need for package sources
    * Do not update existing file for repository definition. Use new list-file
    * Multiverse repo in enabled in Bionic image
    * Do not add same files twice
    * Change script permissions as soon as we added it
    * Drop Xst bits from scripts
    * Repository cache should be fetched before installing anything
    * Inotify-tools could be installed later
    * wget needs ca-certificates to verify https certificate. apt-key needs gnupg
    * System upgrade could be postponed
    * During build process errors are the only interesting thing
    * Less visual distruption during build
    * No need for sudo. Default ubuntu:18.04 user is root
    * Wget is already installed
    * libav-tools is replaced with ffmpeg
    * PHP packaged are updated and renamed a bit
    * libavcodec-extra is meta-package that includes libavcodec-extra* packages
    * Cron is not bundled by default, need to install it
    * Use ubuntu-bundled composer
    * Include libev-libevent-dev for further use
    * Skip separate sources download step and combine it with unpacking
    * Combine two rm commands in one
    * Remove one more rm command
    * Clean unused packages
    * Do not install recommended packages
    * Drop privileges before calling composer (due security risks)
    * Clean unused files
    * Default MySQL config placing socket (and it's lockfile) under '/var/run/mysqld' directory
    * Use user crontab. Not system
    * Squash all RUN commands into one for the sake of image size
    * DocumentRoot needs to be redifined
    * Add recommended PHP modules
    * chown & chmod /var/www

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ VOLUME_HOME="/var/lib/mysql"
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
     echo "=> Installing MySQL ..."
-    mysql_install_db > /dev/null 2>&1
+    mysqld --defaults-file=/etc/mysql/my.cnf --initialize-insecure
     echo "=> Done!"
     /create_mysql_admin_user.sh
 else
