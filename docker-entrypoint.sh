042c0162f6a2c78bf4daf7170e40f4d0e220be71
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2021-02-12 00:15:55 +0100

    Create ampache log folder and make it writeable (#58)
    By default there is no log folder for ampache to write its logs into.
    It first has to be created and ampache (running in apache as the www-data user) has to be given the right to write into the folder.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,8 @@ fi
 # Re-set permission to the `www-data` user if current user is root
 # This avoids permission denied if the data volume is mounted by root
 if [ "$1" = '/run.sh' ] && [ "$(id -u)" = '0' ]; then
-    chown -R www-data:www-data /var/www/config
+    mkdir -p /var/logs/ampache
+    chown -R www-data:www-data /var/www/config /var/logs/ampache
     exec gosu www-data "$@"
 else
   exec "$@"
