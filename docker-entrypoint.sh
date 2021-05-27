6ac7a08cc5354c446fab2d7db73f6a2ec325291b
lachlan <lachlan.00@gmail.com>
2021-05-27 10:25:42 +1000

    Update docker-entrypoint.sh
    log dir incorrect

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,8 +13,8 @@ fi
 # Re-set permission to the `www-data` user if current user is root
 # This avoids permission denied if the data volume is mounted by root
 if [ "$1" = '/run.sh' ] && [ "$(id -u)" = '0' ]; then
-    mkdir -p /var/logs/ampache
-    chown -R www-data:www-data /var/www/config /var/logs/ampache
+    mkdir -p /var/log/ampache
+    chown -R www-data:www-data /var/www/config /var/log/ampache
     exec gosu www-data "$@"
 else
   exec "$@"
