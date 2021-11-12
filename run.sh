085ec1e9a4d3710d625827e7936db1f304566d0d
Kuzma Fesenko <kuzimoto@gmail.com>
2021-11-12 17:05:46 -0600

    fix run.sh (#79)

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ else
 fi
 
 # Copy Ampache config .dist files
-cp /var/tmp/* /var/www/config/
+cp /var/tmp/*.dist /var/www/config/
 
 # Start Supervisor to manage all the processes
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
